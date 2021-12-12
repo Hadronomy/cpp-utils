5df79ce9113be37414716ef4a6cbb46a5b9e661b
Pablo Hernández <hadronomy@gmail.com>
2021-12-12 20:06:09 +0000

    Fixed negative numbers being interpreted as flags

--- test/main.cc
+++ test/main.cc
@@ -6,13 +6,15 @@
 
 int main(int arg_count, char* pased_args[]) {
 	utils::Cli cli(
-		"test",
-		{utils::Argument("test-argument",  "Just an argument, nothing out of the ordinary...")},
-		{utils::Flag("holi", "l", "HOLI")}
+		"Test",
+		"test description",
+		{
+			utils::Argument("test-argument",  "Just an argument, nothing out of the ordinary...")
+		},
+		{
+			utils::Flag("holi", "l", "HOLI")
+		}
 	);
 	cli.Parse(arg_count, pased_args);
-	if (cli.GetFlag("help")) {
-		cli.ShowHelp();
-	}
 	return 0;
 }
