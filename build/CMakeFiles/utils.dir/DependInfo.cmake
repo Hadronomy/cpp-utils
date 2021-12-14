395daeb42dabeac77f4d76bf78b510e3b8c44308
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2021-12-14 00:02:54 +0000

    Modified project to use cmake

---
+++ build/CMakeFiles/utils.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hadronomy/repos/cpp-utils/src/cli/cli.cc" "/home/hadronomy/repos/cpp-utils/build/CMakeFiles/utils.dir/src/cli/cli.cc.o"
+  "/home/hadronomy/repos/cpp-utils/src/colors/colors.cc" "/home/hadronomy/repos/cpp-utils/build/CMakeFiles/utils.dir/src/colors/colors.cc.o"
+  "/home/hadronomy/repos/cpp-utils/src/hourglass/hourglass.cc" "/home/hadronomy/repos/cpp-utils/build/CMakeFiles/utils.dir/src/hourglass/hourglass.cc.o"
+  "/home/hadronomy/repos/cpp-utils/src/misc/misc.cc" "/home/hadronomy/repos/cpp-utils/build/CMakeFiles/utils.dir/src/misc/misc.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../includes/utils"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
