e4f73a22ee8ca44eac3535bd5931b74287217471
Pablo Hernández <hadronomy@gmail.com>
2022-08-08 21:05:41 +0100

    Add `UTILS_LIB` compiler definition & Remove CMake cache

--- .cmake/Config.cmake
+++ .cmake/Config.cmake
@@ -3,3 +3,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@Targets.cmake")
 
 check_required_components(@PROJECT_NAME@)
+string(TOUPPER "@PROJECT_NAME@_LIB" lib_name)
+add_compile_definitions("${lib_name}")
--- example/main.cc
+++ example/main.cc
@@ -4,14 +4,16 @@
 
 #include <iostream>
 
+#ifdef UTILS_LIB
 #include <utils/colors.h>
 #include <utils/hourglass.h>
+#endif
 
 int main() {
+#ifdef UTILS_LIB
   utils::Hourglass::Start();
-#ifdef UTILS
   std::cout << utils::Colorize(utils::ColorTint::kCyan) << "Hello World! " << utils::Colorize::Reset << std::endl;
-#endif
   std::cout << utils::Hourglass::Stop();
+#endif
   return 0;
 }
