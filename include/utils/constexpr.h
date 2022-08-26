d9d17635d6dc6aa9a6b082274f1d9c3156241b0b
Pablo Hernández <hadronomy@gmail.com>
2022-08-26 04:25:05 +0100

    Move ShiftCopyBit to `constexpr.h`
    Also:
    - Add missing include into constexpr.h
    - Remove header guards in place of `#pragma once`
    - Add newline at the end of file :smile:

--- include/utils/constexpr.h
+++ include/utils/constexpr.h
@@ -9,10 +9,10 @@
  * 
  */
 
-#ifndef LIB_UTILS_CONSTEXPR_
-#define LIB_UTILS_CONSTEXPR_
+#pragma once
 
 #include <type_traits>
+#include <string>
 
 namespace utils {
 
@@ -21,6 +21,21 @@ struct is_standard {
   static constexpr bool value = std::is_fundamental<TTest>::value || std::is_same<TTest, std::string>::value;
 };
 
+/**
+ * @brief
+ * Repeats the process of shifting and copying the
+ * given bits an specified amount of times
+ * @tparam TInt
+ * @param initial The bits to shift and copy
+ * @param amount The amount of times to shift and copy
+ * @example
+ * ShiftCopyBit<uint8_t>(1, 4) => 0x00001111;
+ * @return
+ */
+template<class TInt>
+constexpr TInt ShiftCopyBit(TInt initial, size_t amount) {
+  for (int i = 0; i < amount; initial |= initial << i, ++i);
+  return initial;
 }
 
-#endif  //  !LIB_UTILS_CONSTEXPR_
+}
--- include/utils/misc.h
+++ include/utils/misc.h
@@ -101,21 +101,4 @@ bool IsAlpha(const char input_char);
  */
 bool IsUppercase(const char input_char);
 
-/**
- * @brief
- * Repeats the process of shifting and copying the
- * given bits an specified amount of times
- * @tparam TInt
- * @param initial The bits to shift and copy
- * @param amount The amount of times to shift and copy
- * @example
- * ShiftCopyBit<uint8_t>(1, 4) => 0x00001111;
- * @return
- */
-template<class TInt>
-constexpr TInt ShiftCopyBit(TInt initial, size_t amount) {
-  for (int i = 0; i < amount; initial |= initial << i, ++i);
-  return initial;
-}
-
 }  // namespace utils
